33cce92b6ab72306dde17d7464761f712e1df53a
PratikGhoshCrescentek <pratikghosh@crescentek.com>
2024-11-19 17:57:33 +0530

    to removed the helmet provider from Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { renderToNodeStream } from 'react-dom/server';
 import "./Home.css";
@@ -145,20 +145,18 @@ function Home() {
     return <Loader />;
   }
 
-  const helmetContext = {};
 
   return (
-    <HelmetProvider context={helmetContext}>
+    <>
     <Helmet 
-//     onChangeClientState={newState => {
-// // Add description
-// const metaDescription = document.querySelector(
-// 'meta[name="og:description"]'
-// );
-// if (metaDescription) {
-// metaDescription.setAttribute('content', 'Discover a world of events with Twotixx! Enjoy seamless ticket purchases with our innovative digital, NFT, and personalised ticket options. Experience secure, transparent ticketing with low fees today.');
-// }
-// }}
+    onChangeClientState={newState => {
+const metaDescription = document.querySelector(
+'meta[name="og:description"]'
+);
+if (metaDescription) {
+metaDescription.setAttribute('content', 'Discover a world of events with Twotixx! Enjoy seamless ticket purchases with our innovative digital, NFT, and personalised ticket options. Experience secure, transparent ticketing with low fees today.');
+}
+}}
 >
         <title>Twotixx | Your Gateway to Exciting Events with Smart Digital Ticketing.</title>
         <link rel="canonical" href="https://twotixx-meta-test.vercel.app/" />
@@ -502,7 +500,7 @@ function Home() {
         <Footer />
       </div>
     </div>
-    </HelmetProvider>
+    </>
   );
 }
 
