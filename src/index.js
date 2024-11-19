2d7a5d03ba3503d22024cec1cfbf712b11b6b31e
PratikGhoshCrescentek <pratikghosh@crescentek.com>
2024-11-19 18:41:21 +0530

    to added new package react-snap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+// root.render(
+//     <App />
+// );
 reportWebVitals();
 
